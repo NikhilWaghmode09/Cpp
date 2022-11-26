df62ed75dfa50e4ea490ca355f59c871f3472755
Nikhil Sunil Waghmode <nick13waghmode@gmail.com>
2022-11-27 01:26:38 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,25 @@
+/*
+Pattern 2
+"
+*****
+****
+***
+**
+*
+"
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=n;i>0;i--){
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
