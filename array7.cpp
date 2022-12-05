eedbc72d273ef84321fae034ec95376960834b33
Nikhil Sunil Waghmode <nick13waghmode@gmail.com>
2022-12-05 09:28:39 +0530

    Update array7.cpp

--- array7.cpp
+++ array7.cpp
@@ -2,29 +2,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool search(int arr[],int size,int n){
+bool search(int arr[],int size,int key){
     bool temp=0;
     for(int i=0;i<size;i++){
-        if(arr[i]==n){
+        if(arr[i]==key){
             temp=1;
-            //cout<<"\nElement found at"<<i<<"index\n";
-        }
-        else{
-            //cout<<"Element not found:/";
         }
     }
     return temp;
 }
 
 int main(){
-    int size,find;
-    cin>>size>>find;
-    int arr1[10]={1,2,3,4,5,6,7,8,9,0};
-    //check is 0 is present or not.
-    cout<<search(arr1,size,find);
-    
-    
-    
-    
+    int find;
+    cout<<"Enter the element which is to be found: ";
+    cin>>find;
+
+    int arr1[1000]={1,2,3,4,5,6,7,8,9,0};
+    //check if 0 is present or not.
+    bool found = search(arr1,10,find);
+    if(found){
+        cout<<"\nElement found.";
+    }
+    else{
+        cout<<"Element not found:/";
+    }
+
     return 0;
 }
